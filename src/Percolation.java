bd70d439afafc966b2cdf8f014c5e22687bad87b
Kai Pak <kai@kaipak.org>
2019-03-27 08:57:20 -0700

    Start of percolation app.

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,30 +1,47 @@
-public class Percolation {
-    public Percolation(int n) {
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.Arrays;
+
+public class Percolation {
+    private int[][] Grid;
+
+    public Percolation(int n) {
+        int[][] Grid = new int[n][n];
+        WeightedQuickUnionUF UFMap = new WeightedQuickUnionUF(n);
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                Grid[i][j] = i * n + j;
+
+        System.out.println(Arrays.deepToString(Grid));
     }
 
     public void open(int row, int col) {
-
+        return;
     }
 
     public boolean isOpen(int row, int col) {
-
+        return true;
     }
 
     public boolean isFull(int row, int col) {
+
+        return true;
 
     }
 
     public int numberOfOpenSites() {
 
+        return 2;
     }
 
     public boolean percolates() {
 
+        return true;
     }
 
     public static void main(String[] args) {
-
+        Percolation percolation = new Percolation(4);
     }
 
 }
