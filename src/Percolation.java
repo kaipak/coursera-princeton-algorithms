3f89305a6e446e82b6db74663e35e6add8031696
Kai Pak <kai@kaipak.org>
2019-03-30 21:37:34 -0700

    Complete most methods for Percolates class. Need to just finish percolates().

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,48 +1,110 @@
+/* *****************************************************************************
+ *  Name:    Kai Pak
+ *
+ *  Description: Determines if nxn grid object percolates, i.e., has an open
+ *               path from top of grid to bottom. Implements weighted quick
+ *               union find to keep track of which elements are connected
+ *               together, and ultimately, which paths percolate.
+ *
+ *  Written:       03/10/2019
+ *
+ *  % javac Percolation.java
+ *  % java Percolation
+ *  Hello, World
+ *
+ **************************************************************************** */
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
 import java.util.Arrays;
+import java.lang.IllegalArgumentException;
 
 public class Percolation {
-    private int[][] Grid;
-    private WeightedQuickUnionUF GridMap;
+    private int gridSize;
+    private int openSites;
+    private int[][] grid;
+    private WeightedQuickUnionUF gridMap;
 
     public Percolation(int n) {
-        Grid = new int[n][n];
-        GridMap = new WeightedQuickUnionUF(n);
-
+       /* As API requirements state that the grid should start with (1, 1), we
+        * initialize n + 1 rows and columns.
+        *
+        */
+        grid = new int[n + 1][n + 1];
+        gridSize = n;
+        openSites = 0;
+        gridMap = new WeightedQuickUnionUF(n * n);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                Grid[i][j] = i * n + j;
-
-        System.out.println(Arrays.deepToString(Grid));
+                grid[i][j] = 0;
     }
 
-    public void open(int row, int col) {
-        return;
+    private int xyTo1D(int m, int n) {
+        return (m - 1) * gridSize + n;
     }
 
-    public boolean isOpen(int row, int col) {
-        return true;
+    private void throwsOutOfBoundsException(int m, int n) {
+        if (m <= 0 || m > gridSize || n <= 0 || n > gridSize) {
+            throw new IllegalArgumentException("Argument out of bounds.");
+        }
     }
 
-    public boolean isFull(int row, int col) {
+    public void open(int m, int n) {
+        throwsOutOfBoundsException(m, n);
+        grid[m][n] = 1;
+        openSites++;
+        int siteX = xyTo1D(m, n);
+        // Now need to connect open adjacent sites
+        if (m - 1 > 0 && isOpen(m - 1, n)) {
+            int siteY = xyTo1D(m - 1, n);
+            gridMap.union(siteX, siteY);
+        }
+        if (m + 1 <= gridSize && isOpen(m + 1, n)) {
+            int siteY = xyTo1D(m + 1, n);
+            gridMap.union(siteX, siteY);
+        }
+        if (n - 1 > 0 && isOpen(m, n - 1)) {
+            int siteY = xyTo1D(m, n - 1);
+            gridMap.union(siteX, siteY);
+        }
+        if (n + 1 <= gridSize && isOpen(m, n + 1)) {
+            int siteY = xyTo1D(m, n + 1);
+            gridMap.union(siteX, siteY);
+        }
+    }
 
-        return true;
+    public boolean isOpen(int m, int n) {
+        throwsOutOfBoundsException(m, n);
 
+        if(grid[m][n] == 1)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isFull(int m, int n) {
+        throwsOutOfBoundsException(m, n);
+
+        if(grid[m][n] == 0)
+            return true;
+        else
+            return false;
     }
 
     public int numberOfOpenSites() {
-
-        return 2;
+        return openSites;
     }
 
     public boolean percolates() {
-
         return true;
     }
 
     public static void main(String[] args) {
         Percolation percolation = new Percolation(4);
+        percolation.open(1, 1);
+        percolation.open(1, 2);
+        percolation.open(2, 2);
+        percolation.open(4, 4);
+        System.out.println(percolation.numberOfOpenSites());
+        System.out.println(Arrays.deepToString(percolation.grid));
     }
 
 }
