203e908778d8287b3c2e0e6dcd6250713de0632b
Kai Pak <kai@kaipak.org>
2019-04-21 20:12:13 -0700

    Begin Permutation client.

---
+++ deques_and_randomized_queues/src/Permutation.java
@@ -0,0 +1,18 @@
+/* *****************************************************************************
+ *  Name:    Kai Pak
+ *
+ *  Description: Client class for RandomizedQueue
+ *
+ *
+ *  Written:       04/19/2019
+ *
+ *  % javac RandomizedQueue.java
+ *
+ ******************************************************************************/
+
+public class Permutation {
+    public static void main(String[] args) {
+
+
+    }
+}
