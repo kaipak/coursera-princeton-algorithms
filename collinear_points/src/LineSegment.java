8067ef128e880aebd5f1da940cbd398f3d243c20
Kai Pak <kai@kaipak.org>
2019-11-04 07:46:21 -0800

    Add project for collinear points.

---
+++ collinear_points/src/LineSegment.java
@@ -0,0 +1,7 @@
+public class LineSegment {
+    public LineSegment(Point p, Point q)        // constructs the line segment between points p and q
+
+    public void draw()                        // draws this line segment
+
+    public String toString()                    // string representation
+}
